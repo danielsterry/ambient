8070b6be9ce1b5ab042128a178f6c18b5a512e21
Daniel Sterry <danielsterry@dans-mbp.local>
2017-07-04 11:41:41 +1000

    Initial Commit

---
+++ Ambient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ambient
+//
+//  Created by Daniel Sterry on 4/7/17.
+//  Copyright © 2017 IE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
